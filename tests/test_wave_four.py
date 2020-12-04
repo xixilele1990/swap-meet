ef7c39c0171bc670c13155d3311ac6a7b5b3b781
simon del rosario <simon@adadevelopersacademy.org>
2020-12-04 15:00:31 -0800

    wave 4 and 5 tests

---
+++ tests/test_wave_four.py
@@ -0,0 +1,31 @@
+import pytest
+from swap_meet.main import Vendor, Item, Clothing, Decor, Electronics
+
+def test_clothing_has_default_category_and_to_str():
+    cloth = Clothing()
+    assert cloth.category is "Clothing"
+    assert str(cloth) == "The finest clothing you could wear."
+
+
+def test_decor_has_default_category_and_to_str():
+    decor = Decor()
+    assert decor.category is "Decor"
+    assert str(decor) == "Something to decorate your space."
+
+
+def test_electronics_has_default_category_and_to_str():
+    electronics = Electronics()
+    assert electronics.category is "Electronics"
+    assert str(electronics) == "A gadget full of buttons and secrets."
+
+
+def test_items_have_condition_as_float():
+    items = [
+        Clothing(condition=3.5),
+        Decor(condition=3.5),
+        Electronics(condition=3.5)
+    ]
+    for item in items:
+        assert item.condition == pytest.approx(3.5)
+
+
