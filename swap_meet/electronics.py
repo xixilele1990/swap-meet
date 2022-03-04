702c328228872f077527648b5214ecc9ce17d4b6
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-04 10:34:54 -0800

    REmove solution

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,12 +1,2 @@
-from swap_meet.item import Item
-
-class Electronics(Item):
-    def __init__(self, condition=0):
-        super().__init__(condition, category='Electronics')
-        #Item(condition, category="Electronics")
-        #self.condition = condition
-        #self.category = "Electronics"
-
-    def __str__(self):
-        return "A gadget full of buttons and secrets."
-
+class Electronics:
+    pass
